01bb9db46cc8a1b2593174889e919b51e080ca91
Rene Dohmen (imac) <acidjunk@gmail.com>
2020-04-10 16:25:40 +0200

    Updates

---
+++ precache-manifest.0b7a72ec6731f503a172f8906b685d3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76e260b13b0a99e2a52940b181e40fd",
+    "url": "/drill-gui/index.html"
+  },
+  {
+    "revision": "6d11a0457ab1bca3e884",
+    "url": "/drill-gui/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "163d6498a845fb1ed278",
+    "url": "/drill-gui/static/js/2.18d2157f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drill-gui/static/js/2.18d2157f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d11a0457ab1bca3e884",
+    "url": "/drill-gui/static/js/main.866735bb.chunk.js"
+  },
+  {
+    "revision": "b2d99d4895bcdfa09f3d",
+    "url": "/drill-gui/static/js/runtime-main.a18e7567.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drill-gui/static/media/logo.5d5d9eef.svg"
+  }
+]);
